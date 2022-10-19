5055fc9468dd5b60bbba5d38464519380a25ec76
guerrjm1 <98247822+guerrjm1@users.noreply.github.com>
2022-10-18 20:57:51 -0500

    Activity 5

--- server.js
+++ server.js
@@ -2,84 +2,105 @@ import express from 'express';
 import connectDatabase from './config/db';
 import { check, validationResult } from 'express-validator';
 import cors from 'cors';
-import bcrypt  from 'bcryptjs';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import config from 'config';
 import User from './models/User';
 
-// Initialize express application 
+// Initialize express application
 const app = express();
 
-//Connect database
+// Connect database
 connectDatabase();
 
-// Configure Middleware 
-app.use(express.json({ extended: false}));
+// Configure Middleware
+app.use(express.json({ extended: false }));
 app.use(
-    cors({
-        origin: 'http://localhost:3000'
-    })
+  cors({
+    origin: 'http://localhost:3000'
+  })
 );
 
-// API endpoints 
+// API endpoints
 /**
- *   @route GET /
- *   @desc  Test endpoint   
+ * @route GET /
+ * @desc Test endpoint
  */
-app.get('/', (req,res) =>
-    res.send('http get request sent to root api endpoint')
+app.get('/', (req, res) =>
+  res.send('http get request sent to root api endpoint')
 );
 
+app.get('/api/', (req, res) => res.send('http get request sent to api'));
+
 /**
- *   @route POST api/users 
- *   @desc  Register user 
+ * @route POST api/users
+ * @desc Register user
  */
-app.post('/api/users', 
-[
+app.post(
+  '/api/users',
+  [
     check('name', 'Please enter your name')
-    .not()
-    .isEmpty(),
-    check('email', 'Please enter a valid emial').isEmail(),
+      .not()
+      .isEmpty(),
+    check('email', 'Please enter a valid email').isEmail(),
     check(
-        'password',
-        'Please enter a password with 6 or more characrters'
-    ).isLength({ min: 6})
-],
-async (req,res) => {
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 })
+  ],
+  async (req, res) => {
     const errors = validationResult(req);
-    if (!errrors.isEmpty()) {
-        return res.status(422).json({ errors: errors.array()});
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
     } else {
-        const { name, email, password } = req.body;
-        try {
-            // Check if user exists
-            let user = await User.findOne({email: email });
-            if (user) {
-                return res
-                    .status(400)
-                    .json ({ errors: [{ msg: "User already exists" }] });
-                
-            }
-
-            // Create a new user
-            user =  new User ({
-                name: name,
-                email: email,
-                password: password
-            });
-
-            // Encrypt the password
-            const salt = await bcrypt.getSalt (10);
-            user.password = await bcrypt.hash(password, salt);
-
-            // Save to db(database) and return
-            await user.save();
-            res.send("User succesfully registered");
-        } catch (error) {
-            res.status(500).send("Server error");
+      const { name, email, password } = req.body;
+      try {
+        // Check if user exists
+        let user = await User.findOne({ email: email });
+        if (user) {
+          return res
+            .status(400)
+            .json({ errors: [{ msg: 'User already exists' }] });
         }
-        //return res.send(req.body);
+
+        // Create a new user
+        user = new User({
+          name: name,
+          email: email,
+          password: password
+        });
+
+        // Encrypt the password
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+
+        // Save to the db and return
+        await user.save();
+
+        // Generate and return a JWT token
+        const payload = {
+          user: {
+            id: user.id
+          }
+        };
+
+        jwt.sign(
+          payload,
+          config.get('jwtSecret'),
+          { expiresIn: '10hr' },
+          (err, token) => {
+            if (err) throw err;
+            res.json({ token: token });
+          }
+        );
+      } catch (error) {
+        res.status(500).send('Server error');
+      }
     }
-    }
+  }
 );
-// Connection listener 
+
+// Connection listener
 const port = 5000;
 app.listen(port, () => console.log(`Express server running on port ${port}`));
+
